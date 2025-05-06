fbe971146049e88539fa1d517d85c918d2cf79f5
Oleg Shulyakov <olegshulyakov@users.noreply.github.com>
2025-05-06 23:55:25 +0300

    Update CardGrid

--- src/components/AnimatedCardGrid.tsx
+++ src/components/AnimatedCardGrid.tsx
@@ -1,29 +1,38 @@
-import React, { useContext } from "react";
+import React, { useContext, useMemo } from "react";
 import { motion } from "framer-motion";
 import { SearchContext } from "../context/SearchContext";
 import BookmarkCard from "./BookmarkCard";
 import FolderCard from "./FolderCard";
-import { Bookmark, Folder } from "../types";
+import { Folder } from "../types";
 
 const AnimatedCardGrid: React.FC = () => {
   const { filteredBookmarks, setSelectedFolder } = useContext(SearchContext)!;
+  const folders = useMemo(() => filteredBookmarks.filter((item) => item.type === "folder"), [filteredBookmarks]);
+  const bookmarks = useMemo(() => filteredBookmarks.filter((item) => item.type === "link"), [filteredBookmarks]);
 
   const handleFolderClick = (folder: Folder) => {
     setSelectedFolder(folder.title);
   };
 
-  const renderItems = (items: (Folder | Bookmark)[]) => {
-    return items.map((item, index) => {
-      if (item.type === "folder") {
-        return <FolderCard key={index} folder={item} onClick={() => handleFolderClick(item)} />;
-      }
-      return <BookmarkCard key={index} bookmark={item} />;
-    });
-  };
-
   return (
     <motion.div className="card-grid" layout>
-      {renderItems(filteredBookmarks)}
+      {folders.length > 0 && (
+        <div className="folder-grid">
+          {folders.map((item) => (
+            <FolderCard key={item.title} folder={item} onClick={() => handleFolderClick(item)} />
+          ))}
+        </div>
+      )}
+
+      {folders.length > 0 && bookmarks.length > 0 && <hr className="divider"></hr>}
+
+      {bookmarks.length > 0 && (
+        <div className="bookmark-grid">
+          {bookmarks.map((item) => (
+            <BookmarkCard key={item.url} bookmark={item} />
+          ))}
+        </div>
+      )}
     </motion.div>
   );
 };
