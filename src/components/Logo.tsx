ee4225ab6de92f130651c5c8295c936d7b09ba96
Oleg Shulyakov <olegshulyakov@users.noreply.github.com>
2025-05-07 17:21:03 +0300

    Remove extra CSS selectors

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,8 +3,10 @@ import React from "react";
 const Logo: React.FC = () => {
   return (
     <div className="app-logo">
-      <img className="logo-img" src="assets/logo.svg" alt="NeatMark"></img>
-      <a href="">Neatmark</a>
+      <img className="app-logo-img" src="assets/logo.svg" alt="NeatMark"></img>
+      <a className="app-logo-name" href="">
+        Neatmark
+      </a>
     </div>
   );
 };
