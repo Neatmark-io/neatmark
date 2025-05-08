713348b986c19accab49e26cce154305e3b048b5
Oleg Shulyakov <olegshulyakov@users.noreply.github.com>
2025-05-09 00:02:35 +0300

    FolderTree refactor

--- src/components/FolderTree.tsx
+++ src/components/FolderTree.tsx
@@ -4,8 +4,7 @@ import { SearchContext } from "../context/SearchContext";
 import { ThemeContext } from "../context/ThemeContext";
 import { Folder } from "../types";
 
-const FolderTree: React.FC = () => {
-  const { bookmarks } = useContext(BookmarkContext)!;
+const FolderRow: React.FC<{ folder: Folder }> = ({ folder }) => {
   const { selectedFolder, setSelectedFolder } = useContext(SearchContext)!;
   const { hideSidebar } = useContext(ThemeContext)!;
 
@@ -14,19 +13,23 @@ const FolderTree: React.FC = () => {
     hideSidebar();
   };
 
-  const renderFolder = (folder: Folder) => (
-    <li key={folder.title} className={`folder ${selectedFolder === folder.title ? "selected" : ""}`}>
+  return (
+    <li className={`folder ${selectedFolder === folder.title ? "selected" : ""}`}>
       <div className="title" onClick={() => handleFolderClick(folder)}>
         <span className={`icon ${folder.icon ? "" : "default"}`}>{folder.icon}</span>
         <span className="folder-name">{folder.title}</span>
       </div>
-      <ul>{folder.children.map((child) => child.type === "folder" && renderFolder(child))}</ul>
+      <ul>{folder.children.map((child) => child.type === "folder" && <FolderRow folder={child} />)}</ul>
     </li>
   );
+};
+
+const FolderTree: React.FC = () => {
+  const { bookmarks } = useContext(BookmarkContext)!;
 
   return (
     <nav className="folder-tree">
-      <ul>{bookmarks.map((item) => item.type === "folder" && renderFolder(item))}</ul>
+      <ul>{bookmarks.map((item) => item.type === "folder" && <FolderRow key={item.title} folder={item} />)}</ul>
     </nav>
   );
 };
