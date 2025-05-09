5cd3a20406520846568d1ee269728dac4cd644e8
Oleg Shulyakov <olegshulyakov@users.noreply.github.com>
2025-05-09 13:29:00 +0300

    Split CardGrid components

--- src/components/CardGrid.tsx
+++ src/components/CardGrid.tsx
@@ -1,27 +1,50 @@
 import React, { useContext, useMemo } from "react";
 import { motion } from "framer-motion";
 import { SearchContext } from "../context/SearchContext";
-import { Folder } from "../types";
+import { Bookmark, Folder } from "../types";
 
-const CardGrid: React.FC = () => {
-  const { filteredBookmarks, setSelectedFolder } = useContext(SearchContext)!;
-  const folders = useMemo(() => filteredBookmarks.filter((item) => item.type === "folder"), [filteredBookmarks]);
-  const bookmarks = useMemo(() => filteredBookmarks.filter((item) => item.type === "link"), [filteredBookmarks]);
+const FolderCard: React.FC<{ folder: Folder }> = ({ folder }) => {
+  const { setSelectedFolder } = useContext(SearchContext)!;
 
   const handleFolderClick = (folder: Folder) => {
     setSelectedFolder(folder.title);
   };
 
   return (
+    <div key={folder.title} className="folder-card" onClick={() => handleFolderClick(folder)}>
+      <span className="icon" />
+      <h3 className="title">{folder.title}</h3>
+    </div>
+  );
+};
+
+const BookmarkCard: React.FC<{ bookmark: Bookmark }> = ({ bookmark }) => (
+  <a key={bookmark.url} href={bookmark.url} target="_blank" rel="noopener noreferrer">
+    <div className="bookmark-card">
+      {!bookmark.icon && <span className="icon" />}
+      {bookmark.icon && <img src={bookmark.icon} alt={bookmark.title} className="icon-image" />}
+
+      <div className="info">
+        <h3 className="title">{bookmark.title}</h3>
+        <p className="url">{bookmark.url}</p>
+      </div>
+
+      {/* {bookmark.meta?.description && <p className="text-sm mt-2">{bookmark.meta.description}</p>} */}
+    </div>
+  </a>
+);
+
+const CardGrid: React.FC = () => {
+  const { filteredBookmarks } = useContext(SearchContext)!;
+  const folders = useMemo(() => filteredBookmarks.filter((item) => item.type === "folder"), [filteredBookmarks]);
+  const bookmarks = useMemo(() => filteredBookmarks.filter((item) => item.type === "link"), [filteredBookmarks]);
+
+  return (
     <motion.div className="card-grid" layout>
       {folders.length > 0 && (
         <div className="folder-grid">
           {folders.map((folder) => (
-            // FolderCard
-            <div key={folder.title} className="folder-card" onClick={() => handleFolderClick(folder)}>
-              <span className="icon" />
-              <h3 className="title">{folder.title}</h3>
-            </div>
+            <FolderCard folder={folder} />
           ))}
         </div>
       )}
@@ -31,20 +54,7 @@ const CardGrid: React.FC = () => {
       {bookmarks.length > 0 && (
         <div className="bookmark-grid">
           {bookmarks.map((bookmark) => (
-            // BookmarkCard
-            <a key={bookmark.url} href={bookmark.url} target="_blank" rel="noopener noreferrer">
-              <div className="bookmark-card">
-                {!bookmark.icon && <span className="icon" />}
-                {bookmark.icon && <img src={bookmark.icon} alt={bookmark.title} className="icon-image" />}
-
-                <div className="info">
-                  <h3 className="title">{bookmark.title}</h3>
-                  <p className="url">{bookmark.url}</p>
-                </div>
-
-                {/* {bookmark.meta?.description && <p className="text-sm mt-2">{bookmark.meta.description}</p>} */}
-              </div>
-            </a>
+            <BookmarkCard bookmark={bookmark} />
           ))}
         </div>
       )}
