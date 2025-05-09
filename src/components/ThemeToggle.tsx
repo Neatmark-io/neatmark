19626533bafd079b3193bf028e17a04266ab1292
Oleg Shulyakov <olegshulyakov@users.noreply.github.com>
2025-05-09 13:37:21 +0300

    Split ThemeToggle components

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
 import { Theme } from "../types";
 
-const ThemeToggle: React.FC = () => {
+const ThemeButton: React.FC<{ title: Theme }> = ({ title }) => {
   const { theme, setTheme } = useContext(ThemeContext)!;
 
   const handleThemeChange = (newTheme: Theme) => {
@@ -10,29 +10,24 @@ const ThemeToggle: React.FC = () => {
   };
 
   return (
+    <button
+      title={`${title} theme`}
+      onClick={() => handleThemeChange(title)}
+      className={`${theme === title ? "selected" : ""}`}
+    >
+      <div className={`${title}-icon`} />
+    </button>
+  );
+};
+
+const ThemeToggle: React.FC = () => {
+  return (
     <div className="theme-toggle">
-      <button
-        title="System Theme"
-        onClick={() => handleThemeChange("system")}
-        className={`${theme === "system" ? "selected" : ""}`}
-      >
-        <div className="system-icon" />
-      </button>
-      <button
-        title="Light Theme"
-        onClick={() => handleThemeChange("light")}
-        className={`${theme === "light" ? "selected" : ""}`}
-      >
-        <div className="light-icon" />
-      </button>
-      <button
-        title="Dark Theme"
-        onClick={() => handleThemeChange("dark")}
-        className={`${theme === "dark" ? "selected" : ""}`}
-      >
-        <div className="dark-icon" />
-      </button>
+      <ThemeButton title="system" />
+      <ThemeButton title="light" />
+      <ThemeButton title="dark" />
     </div>
   );
 };
+
 export default ThemeToggle;
