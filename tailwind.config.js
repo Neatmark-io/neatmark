64dd8a823ad85db1b0a87146dea749884ed28ca9
Oleg Shulyakov <olegshulyakov@users.noreply.github.com>
2025-08-10 15:49:30 +0300

    DaisyUI draft implementation

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,5 +11,49 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("daisyui")
+  ],
+  daisyui: {
+    themes: [
+      "all",
+      {
+        notion: {
+          "color-scheme": "light",
+          "primary": "oklch(44% 0.03 256.802)",
+          "primary-content": "oklch(98% 0 0)",
+          "secondary": "oklch(92% 0.004 286.32)",
+          "secondary-content": "oklch(44% 0.017 285.786)",
+          "accent": "oklch(77% 0.152 181.912)",
+          "accent-content": "oklch(38% 0.063 188.416)",
+          "neutral": "oklch(14% 0.005 285.823)",
+          "neutral-content": "oklch(92% 0.004 286.32)",
+          "base-100": "oklch(100% 0 0)",
+          "base-200": "oklch(98% 0 0)",
+          "base-300": "oklch(95% 0 0)",
+          "base-content": "oklch(44% 0.017 285.786)",
+          "info": "oklch(74% 0.16 232.661)",
+          "info-content": "oklch(29% 0.066 243.157)",
+          "success": "oklch(76% 0.177 163.223)",
+          "success-content": "oklch(37% 0.077 168.94)",
+          "warning": "oklch(82% 0.189 84.429)",
+          "warning-content": "oklch(41% 0.112 45.904)",
+          "error": "oklch(71% 0.194 13.428)",
+          "error-content": "oklch(27% 0.105 12.094)",
+
+          "--rounded-box": "0.5rem",
+          "--rounded-btn": "0.375rem",
+          "--rounded-badge": "0.5rem",
+          "--border-btn": "1px",
+          "--tab-border": "1px",
+          "--tab-radius": "0.25rem",
+        }
+      }
+    ],
+    darkTheme: "dark",
+    base: true, // Applys background color and foreground color for root element by default
+    styled: true, // Include daisyUI colors and design decisions for all components
+    utils: true, // Adds responsive and modifier utility classes
+    logs: true, // Shows info about daisyUI version and used config in the console
+  },
 }
