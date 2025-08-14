a2348a24ea494a12a9996b5b7576c1ef4cf3e32f
Oleg Shulyakov <olegshulyakov@users.noreply.github.com>
2025-08-14 13:40:16 +0300

    feat: pwa updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
-import { StrictMode } from 'react'
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from './App.tsx'
+import { registerSW } from "virtual:pwa-register";
+import App from "./App.tsx";
 import "./index.css";
+
+registerSW({ immediate: true });
 
 /**
  * The root component that renders the application.
  * It uses createRoot to mount the application into the DOM element with the ID 'root'.
  * StrictMode is used for development to identify potential problems in the application.
  */
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,65 +1,64 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name]-[hash].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: function (file) {
-          return file.names.some((name) => name.includes("css")) ? `assets/[name]-[hash].[ext]` : `assets/[name].[ext]`;
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, false);
+
+  return {
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: `assets/[name]-[hash].js`,
+          chunkFileNames: `assets/[name].js`,
+          assetFileNames: function (file) {
+            return file.names.some((name) => name.includes("css"))
+              ? `assets/[name]-[hash].[ext]`
+              : `assets/[name].[ext]`;
+          },
         },
       },
     },
-  },
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      manifest: {
-        name: "Neatmark",
-        short_name: "Neatmark",
-        description: "A Notion-inspired bookmark manager.",
-        display: "standalone",
-        theme_color: "#f4f4f5",
-        background_color: "#f4f4f5",
-        icons: [
-          {
-            src: "assets/favicon.ico",
-            sizes: "48x48",
-            type: "image/x-icon",
-          },
-          {
-            src: "assets/favicon.svg",
-            sizes: "any",
-            type: "image/svg+xml",
-          },
-          {
-            src: "assets/logo192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "maskable",
-          },
-          {
-            src: "assets/logo512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable",
-          },
-        ],
-      },
-      workbox: {
-        cleanupOutdatedCaches: true,
-        globPatterns: ["**/*.{js,css,ico,png,svg}"],
-        runtimeCaching: [
-          {
-            urlPattern: /.*\/data\/.*/,
-            handler: "NetworkFirst",
-          },
-        ],
-      },
-    }),
-  ],
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: "autoUpdate",
+        manifest: {
+          name: "Neatmark",
+          short_name: "Neatmark",
+          description: "A Notion-inspired bookmark manager.",
+          display: "standalone",
+          theme_color: "#f4f4f5",
+          background_color: "#f4f4f5",
+          icons: [
+            { src: "icon512_maskable.png", purpose: "maskable", sizes: "512x512", type: "image/png" },
+            { src: "icon512_rounded.png", purpose: "any", sizes: "512x512", type: "image/png" },
+            { src: "assets/favicon.ico", sizes: "48x48", type: "image/x-icon" },
+            { src: "assets/favicon.svg", sizes: "any", type: "image/svg+xml" },
+          ],
+          screenshots: [
+            { src: "screenshots/desktop.png", sizes: "1280x659", type: "image/png", form_factor: "wide" },
+            { src: "screenshots/mobile.png", sizes: "597x1280", type: "image/png", form_factor: "narrow" },
+          ],
+          start_url: env["BASE_URL"],
+          orientation: "any",
+          lang: "en-US",
+        },
+        workbox: {
+          cleanupOutdatedCaches: true,
+          globPatterns: ["**/*.{html,css,js,ico,svg,png,jpg}"],
+          runtimeCaching: [
+            {
+              urlPattern: /.*\/assets\/.*/,
+              handler: "CacheFirst",
+            },
+            {
+              urlPattern: /.*\/data\/.*/,
+              handler: "NetworkFirst",
+            },
+          ],
+        },
+      }),
+    ],
+  };
 });
