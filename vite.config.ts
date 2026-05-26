694df0f78ae1a43aee82d2e5b32f292e0f45ba9f
Oleg Shulyakov <olegshulyakov@users.noreply.github.com>
2026-05-26 12:55:24 +0300

    Add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,57 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    sveltekit(),
+    SvelteKitPWA({
+      manifest: {
+        name: "Neatmark",
+        short_name: "Neatmark",
+        description: "A Notion-inspired bookmark manager.",
+        display: "standalone",
+        theme_color: "#f4f4f5",
+        background_color: "#f4f4f5",
+        icons: [
+          {
+            src: "assets/favicon.ico",
+            sizes: "48x48",
+            type: "image/x-icon",
+          },
+          {
+            src: "assets/favicon.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+          },
+          {
+            src: "assets/logo192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "assets/logo512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /.*\/data\/.*/,
+            handler: "NetworkFirst",
+          },
+          {
+            urlPattern: /.*\/assets\/.*/,
+            handler: "StaleWhileRevalidate",
+          },
+        ],
+      },
+    }),
+  ],
+});
